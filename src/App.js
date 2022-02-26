5c0964978c5cf2fc98e1852d24b0c0a587815af3
RIHAB KTAIFI <96588974+RihabKT@users.noreply.github.com>
2022-02-26 18:09:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
 
       <div className='flexy'>
 
-     <h1> WELCOME! </h1>
+      <h1> JOIN US, JOIN US!</h1>
+    <img  src=' https://static3.srcdn.com/wordpress/wp-content/uploads/2020/07/Pokemon-Go-Jessie-and-James.jpg?q=50&fit=crop&w=963&h=481&dpr=1.5 ' alt='myimage' width="250"/>
      <button className='login'>LOGIN</button>
      <button className='logout'> LOGOUT </button>
 
